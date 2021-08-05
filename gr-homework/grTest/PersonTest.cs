529647e2815e02e6dce9a79a3cd9b9aeb87cf6bc
Steve Wakeford <steve@stevewakeford.com>
2021-08-05 17:52:24 -0400

    added tests for person equals/hashcode and BuildSetFromLines

--- gr-homework/grTest/PersonTest.cs
+++ gr-homework/grTest/PersonTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using gr;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,60 @@ using System.Threading.Tasks;
 namespace grTest
 {
     [TestClass]
-    class PersonTest
+    public class PersonTest
     {
+        [TestMethod]
+        public void SamePersonIsEqual()
+        {
+            Person p1 = new Person("LastName", "FirstName", "Email", "FavoriteColor", DateTime.Parse("1/1/1900"));
+            Person p2 = new Person("LastName", "FirstName", "Email", "FavoriteColor", DateTime.Parse("1/1/1900"));
+
+            Assert.AreEqual(p1, p2);
+        }
+
+        [TestMethod]
+        public void SamePersonIsEqualWithSomeNulls()
+        {
+            Person p1 = new Person("LastName", null, "Email", "FavoriteColor", DateTime.Parse("1/1/1900"));
+            Person p2 = new Person("LastName", null, "Email", "FavoriteColor", DateTime.Parse("1/1/1900"));
+
+            Assert.AreEqual(p1, p2);
+        }
+
+        [TestMethod]
+        public void SamePersonHasSameHashCode()
+        {
+            Person p1 = new Person("LastName", "FirstName", "Email", "FavoriteColor", DateTime.Parse("1/1/1900"));
+            Person p2 = new Person("LastName", "FirstName", "Email", "FavoriteColor", DateTime.Parse("1/1/1900"));
+
+            Assert.AreEqual(p1.GetHashCode(), p2.GetHashCode());
+        }
+
+        [TestMethod]
+        public void DifferentPersonIsNotEqual()
+        {
+            Person p1 = new Person("LastName", "FirstName", "Email", "FavoriteColor", DateTime.Parse("1/1/1900"));
+            Person p2 = new Person("DifferentLastName", "DifferentFirstName", "Differentmail", "DifferentFavoriteColor", DateTime.Parse("2/2/1902"));
+
+            Assert.AreNotEqual(p1, p2);
+        }
+
+        [TestMethod]
+        public void DifferentPersonHasDifferentHashCode()
+        {
+            Person p1 = new Person("LastName", "FirstName", "Email", "FavoriteColor", DateTime.Parse("1/1/1900"));
+            Person p2 = new Person("DifferentLastName", "DifferentFirstName", "Differentmail", "DifferentFavoriteColor", DateTime.Parse("2/2/1902"));
+
+            Assert.AreNotEqual(p1.GetHashCode(), p2.GetHashCode());
+        }
+
+        [TestMethod]
+        public void NullPersonIsNotEqual()
+        {
+            Person p1 = new Person("LastName", "FirstName", "Email", "FavoriteColor", DateTime.Parse("1/1/1900"));
+            Person p2 = null;
+
+            Assert.AreNotEqual(p1, p2);
+        }
     }
 }
--- gr-homework/grTest/SetFromLinesTest.cs
+++ gr-homework/grTest/SetFromLinesTest.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using gr;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +10,46 @@ using System.Threading.Tasks;
 namespace grTest
 {
     [TestClass]
-    class SetFromLinesTest
+    public class SetFromLinesTest
     {
+        [TestMethod]
+        public void OneLineBuildsSetWithOneItem()
+        {
+            var oneLine = "one line";
+
+            var oneLineSet = SetFromLines.Build(new MemoryStream(Encoding.ASCII.GetBytes(oneLine)), s => s);
+
+            Assert.AreEqual(oneLineSet.Count, 1);
+        }
+
+        [TestMethod]
+        public void TwoLinesBuildsSetWithTwoItems()
+        {
+            var twoLines = "one line\nanother line";
+
+            var twoLinesSet = SetFromLines.Build(new MemoryStream(Encoding.ASCII.GetBytes(twoLines)), s => s);
+
+            Assert.AreEqual(twoLinesSet.Count, 2);
+        }
+
+        [TestMethod]
+        public void ThreeLinesBuildsSetWithThreeItems()
+        {
+            var threeLines = "one line\nanother line\nthird line";
+
+            var threeLinesSet = SetFromLines.Build(new MemoryStream(Encoding.ASCII.GetBytes(threeLines)), s => s);
+
+            Assert.AreEqual(threeLinesSet.Count, 3);
+        }
+
+        [TestMethod]
+        public void ZeroLinesBuildsSetWithZeroItems()
+        {
+            string zeroLines = string.Empty;
+
+            var zeroLinesSet = SetFromLines.Build(new MemoryStream(Encoding.ASCII.GetBytes(zeroLines)), s => s);
+
+            Assert.AreEqual(zeroLinesSet.Count, 0);
+        }
     }
 }
