9f526fcde94e779ead9680dba5a9e5d8227e0a29
Steve Wakeford <steve@stevewakeford.com>
2021-08-05 17:54:59 -0400

    clean up usings

--- gr-homework/grTest/PersonDisplayTest.cs
+++ gr-homework/grTest/PersonDisplayTest.cs
@@ -1,14 +1,11 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using gr;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace grTest
 {
     [TestClass]
-    class PersonDisplayTest
+    public class PersonDisplayTest
     {
     }
 }
