50a71e7f6d14586d256bf5020fc9579d6a28ed9b
Steve Wakeford <steve@stevewakeford.com>
2021-08-05 21:52:23 -0400

    added post and get endpoints

--- gr-homework/grWeb/App_Start/WebApiConfig.cs
+++ gr-homework/grWeb/App_Start/WebApiConfig.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
 
 namespace grWeb
 {
@@ -9,16 +6,15 @@ namespace grWeb
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
-
-            // Web API routes
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+                routeTemplate: "{controller}"
             );
+
+            // force JSON
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
         }
     }
 }
--- gr-homework/grWeb/Controllers/RecordsController.cs
+++ gr-homework/grWeb/Controllers/RecordsController.cs
@@ -1,17 +1,42 @@
-﻿using System;
+﻿using gr;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+using System.Web.Http;
 
 namespace grWeb.Controllers
 {
-    public class RecordsController : Controller
+    public class RecordsController : ApiController
     {
-        // GET: Records
-        public ActionResult Index()
+        private static readonly List<Person> Persons = new List<Person>();
+
+        [Route("records/color")]
+        public IEnumerable<Person> GetSortedByColor()
         {
-            return View();
+            return Persons.OrderBy(p => p.FavoriteColor);
+        }
+
+        [Route("records/birthdate")]
+        public IEnumerable<Person> GetSortedByDateOfBirth()
+        {
+            return Persons.OrderBy(p => p.DateOfBirth);
+        }
+
+        [Route("records/name")]
+        public IEnumerable<Person> GetSortedByLastName()
+        {
+            return Persons.OrderBy(p => p.LastName);
+        }
+
+        public async void Post()
+        {
+            var content = await Request.Content.ReadAsStringAsync();
+
+            var person = Person.Parse(content);
+
+            if (person == null) throw new Exception("Failed to parse");
+
+            Persons.Add(person);
         }
     }
 }
